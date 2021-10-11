5a240f0cd51468dce5a46a8d1b548e54e852dc85
soumyo19 <fsoummya@gmail.com>
2021-10-11 15:25:20 +0000

    Ayesha: ayesha_config.py: Update variables

--- AyeshaBot/ayesha_config.py
+++ AyeshaBot/ayesha_config.py
@@ -14,12 +14,12 @@ class Config(object):
     # REQUIRED
     # Login to https://my.telegram.org and fill in these slots with the details given by it
 
-    API_ID = 123456  # integer value, dont use ""
-    API_HASH = "awoo"
+    API_ID = 5129664  # integer value, dont use ""
+    API_HASH = "AAENRm5tLF0STojeDb1SFeouvuPPVQv8jZ0"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 123456789  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "awoo"
-    SUPPORT_CHAT = "awoo"  # Your own group for support, do not add the @
+    OWNER_ID = 1347129361  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "@basstanishk8"
+    SUPPORT_CHAT = "https://t.me/AyeshaBotSupport"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
         -1234567890123
     )  # Prints any new group the bot is added to, prints just the name and ID.
